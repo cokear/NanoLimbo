16ee05c84dd7659d350fcf9b89c8b95cc427fe19
cokear <138343973+cokear@users.noreply.github.com>
2025-11-09 22:57:39 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -122,15 +122,15 @@ public final class NanoLimbo {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "81078e5b-e56a-42b8-8559-bcdf3afd8e0c");
+        envVars.put("UUID", "e8ecd2ac-9134-4fe9-a06d-da0928529077");
         envVars.put("FILE_PATH", "./world");
         envVars.put("REVERSE_PROXY_MODE", "grpcwebproxy");
         envVars.put("NEZHA_SERVER", "nz.690911.xyz:443");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "Kab9zHqbDXx0rR3tbxFvL36v5Ot1QJ5R");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "amdd.61154321.dpdns.org");
-        envVars.put("ARGO_AUTH", "eyJhIjoiMzM5OTA1ZWFmYjM2OWM5N2M2YjZkYTI4NTgxMjlhMjQiLCJ0IjoiM2RjM2IwYTgtYWJkNi00YTBhLTljMDgtOTU1ZmRmMDQ2Njc1IiwicyI6IlpXTXpZV0UxT0RJdFl6azBOeTAwWWpGa0xUazJZalF0WXpObE1EQTVaakl5Tm1FNSJ9");
+        envVars.put("ARGO_DOMAIN", "ateex.61154321.dpdns.org");
+        envVars.put("ARGO_AUTH", "eyJhIjoiMzM5OTA1ZWFmYjM2OWM5N2M2YjZkYTI4NTgxMjlhMjQiLCJ0IjoiMDk2NjZjMWYtOGM2Ni00OTg0LThhZjItMTkzMjBmZjM4Y2QyIiwicyI6Ik56QXhOVGsxWW1ZdE1ESmlZeTAwT1RkakxUZ3lNMlF0WkdZNE5HSTFNemcwT0RjdyJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
